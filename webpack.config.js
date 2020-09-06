f4179ca9ba319e2eacfe5a88437dfbed3224e87c
Ravina-Deogadkar <ravina.deogadkar@gmail.com>
2020-09-06 20:55:38 +0530

    adds webpack.config file

---
+++ webpack.config.js
@@ -0,0 +1,3 @@
+const client = {    entry: {        'client': './src/client/index.js'    }};
+const server = {    entry: {        'server': './src/server/index.js'    }};
+module.exports = [client, server];
