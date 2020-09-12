e78616b39295069aad2f5c69af146155b3f25f51
Ravina-Deogadkar <ravina.deogadkar@gmail.com>
2020-09-12 12:58:50 +0530

    Feat: adds webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
-const client = {    entry: {        'client': './src/client/index.js'    }};
-const server = {    entry: {        'server': './src/server/index.js'    }};
+const path = require('path');let nodeExternals = require('webpack-node-externals');
+const moduleObj = {    loaders: [        {            test: /\.js$/,            exclude: /node_modules/,            loaders: ["babel-loader"],        }    ],};
+const client = {    entry: {        'client': './src/client/index.js',    },    target: 'web',    output: {        filename: '[name].js',        path: path.resolve(__dirname, 'dist')    },    module: moduleObj};
+const server = {    entry: {        'server': './src/server/index.js'    },    target: 'node',    output: {        filename: '[name].js',        path: path.resolve(__dirname, 'dist')    },    module: moduleObj,externals: [nodeExternals()]};
 module.exports = [client, server];
